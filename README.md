9ba0c2caf586d56868b08b56b0ba2a280f80a2ed
baba_s <shotababa1206@gmail.com>
2020-06-28 12:40:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# UniGetComponentWithLogExtensionMethods
+
+## 使用例
+
+```cs
+using Kogane;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponentWithLogExtensionMethods.OnFailureGetComponent = ( methodName, gameObject, type ) =>
+        {
+            Debug.LogError( $@"「{gameObject.name}」から「{methodName}」で「{type.Name}」を取得できませんでした" );
+        };
+
+        GetComponentWithLogExtensionMethods.OnFailureFind = ( methodName, gameObject, name ) =>
+        {
+            Debug.LogError( $@"「{gameObject.name}」から「{methodName}」で「{name}」を取得できませんでした" );
+        };
+
+        gameObject.GetComponentWithLog<BoxCollider>();
+        gameObject.GetComponentInChildrenWithLog<BoxCollider>();
+        gameObject.GetComponentInChildrenWithLog<BoxCollider>( true );
+        gameObject.GetComponentInChildrenWithoutSelfWithLog<BoxCollider>();
+        gameObject.GetComponentInChildrenWithoutSelfWithLog<BoxCollider>( true );
+        gameObject.GetComponentInParentWithLog<BoxCollider>();
+        gameObject.GetComponentInParentWithoutSelfWithLog<BoxCollider>();
+        gameObject.GetComponentInParentWithoutSelfWithLog<BoxCollider>( true );
+        gameObject.FindWithLog( "Child" );
+        gameObject.FindGameObjectWithLog( "Child" );
+        gameObject.FindComponentWithLog<BoxCollider>( "Child" );
+        gameObject.FindDeepWithLog( "Child" );
+        gameObject.FindDeepWithLog( "Child", true );
+
+        transform.GetComponentWithLog<BoxCollider>();
+        transform.GetComponentInChildrenWithLog<BoxCollider>();
+        transform.GetComponentInChildrenWithLog<BoxCollider>( true );
+        transform.GetComponentInChildrenWithoutSelfWithLog<BoxCollider>();
+        transform.GetComponentInChildrenWithoutSelfWithLog<BoxCollider>( true );
+        transform.GetComponentInParentWithLog<BoxCollider>();
+        transform.GetComponentInParentWithoutSelfWithLog<BoxCollider>();
+        transform.GetComponentInParentWithoutSelfWithLog<BoxCollider>( true );
+        transform.FindWithLog( "Child" );
+        transform.FindGameObjectWithLog( "Child" );
+        transform.FindComponentWithLog<BoxCollider>( "Child" );
+        transform.FindDeepWithLog( "Child" );
+        transform.FindDeepWithLog( "Child", true );
+    }
+}
+```
+
+![2020-06-28_123925](https://user-images.githubusercontent.com/6134875/85937217-72989500-b93c-11ea-97bd-5e5de723f733.png)
---
+++ Scripts/ComponentExt.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+	public static partial class GetComponentWithLogExtensionMethods
+	{
+		//================================================================================
+		// GetComponent
+		//================================================================================
+		/// <summary>
+		/// コンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentWithLog<T>( this Component self ) where T : Component
+		{
+			return self.gameObject.GetComponentWithLog<T>();
+		}
+
+		//================================================================================
+		// GetComponentInChildren
+		//================================================================================
+		/// <summary>
+		/// 自分自身もしくは子からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInChildrenWithLog<T>( this Component self ) where T : Component
+		{
+			return self.gameObject.GetComponentInChildrenWithLog<T>();
+		}
+
+		/// <summary>
+		/// 自分自身もしくは子からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInChildrenWithLog<T>( this Component self, bool includeInactive ) where T : Component
+		{
+			return self.gameObject.GetComponentInChildrenWithLog<T>( includeInactive );
+		}
+
+		//================================================================================
+		// GetComponentInChildrenWithoutSelf
+		//================================================================================
+		/// <summary>
+		/// 子からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInChildrenWithoutSelfWithLog<T>( this Component self ) where T : Component
+		{
+			return self.gameObject.GetComponentInChildrenWithoutSelfWithLog<T>();
+		}
+
+		/// <summary>
+		/// 子からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInChildrenWithoutSelfWithLog<T>( this Component self, bool includeInactive ) where T : Component
+		{
+			return self.gameObject.GetComponentInChildrenWithoutSelfWithLog<T>( includeInactive );
+		}
+
+		//================================================================================
+		// GetComponentInParent
+		//================================================================================
+		/// <summary>
+		/// 自分自身もしくは親からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInParentWithLog<T>( this Component self ) where T : Component
+		{
+			return self.gameObject.GetComponentInParentWithLog<T>();
+		}
+
+		//================================================================================
+		// GetComponentInParentWithoutSelf
+		//================================================================================
+		/// <summary>
+		/// 親からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInParentWithoutSelfWithLog<T>( this Component self ) where T : Component
+		{
+			return self.gameObject.GetComponentInParentWithoutSelfWithLog<T>();
+		}
+
+		/// <summary>
+		/// 親からコンポーネントを取得します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T GetComponentInParentWithoutSelfWithLog<T>( this Component self, bool includeInactive ) where T : Component
+		{
+			return self.gameObject.GetComponentInParentWithoutSelfWithLog<T>( includeInactive );
+		}
+
+		//================================================================================
+		// Find
+		//================================================================================
+		/// <summary>
+		/// 指定された名前の子を検索します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static Transform FindWithLog( this Component self, string name )
+		{
+			return self.gameObject.FindWithLog( name );
+		}
+
+		/// <summary>
+		/// 指定された名前の子を検索します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static GameObject FindGameObjectWithLog( this Component self, string name )
+		{
+			return self.gameObject.FindGameObjectWithLog( name );
+		}
+
+		/// <summary>
+		/// 指定された名前の子からコンポーネントを検索します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static T FindComponentWithLog<T>( this Component self, string name ) where T : Component
+		{
+			return self.gameObject.FindComponentWithLog<T>( name );
+		}
+
+		//================================================================================
+		// FindDeep
+		//================================================================================
+		/// <summary>
+		/// 指定された名前の子を検索します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static GameObject FindDeepWithLog( this Component self, string name )
+		{
+			return self.gameObject.FindDeepWithLog( name );
+		}
+
+		/// <summary>
+		/// 指定された名前の子を検索します。取得できなかった場合はログを出力します
+		/// </summary>
+		public static GameObject FindDeepWithLog( this Component self, string name, bool includeInactive )
+		{
+			return self.gameObject.FindDeepWithLog( name, includeInactive );
+		}
+	}
+}
